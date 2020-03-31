6ffb5e2e6e8ff04d15d17771acce2051af27b748
Васильев Артём Владимирович <artyom@ntpgorizont.ru>
2020-03-31 21:27:02 +0300

    Скрипты теперь без аргументов

--- _export.cmd
+++ _export.cmd
@@ -1,13 +1,14 @@
 @echo off
 rem set SQLITE3_PATH=C:\sqlite
+set file=gorizont
 
 if not exist csv mkdir csv
 
 if exist csv\query.sql del /q csv\query.sql
 
-for /f "usebackq" %%i in (`%SQLITE3_PATH%\sqlite3.exe %1.db "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%%';"`) do (
+for /f "usebackq" %%i in (`%SQLITE3_PATH%\sqlite3.exe %file%.db "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%%';"`) do (
 	echo export %%i
 	echo DROP TABLE IF EXISTS %%i; >> csv\query.sql
-	%SQLITE3_PATH%\sqlite3.exe %1.db ".schema %%i" >> csv\query.sql
-	%SQLITE3_PATH%\sqlite3.exe -noheader -csv %1.db "select * from %%i;" > csv\%%i.csv
+	%SQLITE3_PATH%\sqlite3.exe %file%.db ".schema %%i" >> csv\query.sql
+	%SQLITE3_PATH%\sqlite3.exe -noheader -csv %file%.db "select * from %%i;" > csv\%%i.csv
 )
--- _import.cmd
+++ _import.cmd
@@ -1,92 +1,93 @@
 @echo off
 setlocal enabledelayedexpansion
 rem set SQLITE3_PATH=C:\sqlite
+set file=gorizont
 
-%SQLITE3_PATH%\sqlite3.exe %1.db < csv\query.sql
+%SQLITE3_PATH%\sqlite3.exe %file%.db < csv\query.sql
 
-echo [OutputDatabaseLinkFile] > %1.DbLib
-echo Version=1.1 >> %1.DbLib
-echo [DatabaseLinks] >> %1.DbLib
-for /f %%f in ("%1.db") do (
-	echo ConnectionString=Provider=MSDASQL.1;Persist Security Info=False;Extended Properties="Driver={SQLite3 ODBC Driver};Database=%%~ff;StepAPI=0;SyncPragma=;NoTXN=0;Timeout=;ShortNames=0;LongNames=0;NoCreat=0;NoWCHAR=0;FKSupport=0;JournalMode=;OEMCP=0;LoadExt=;BigInt=0;JDConv=0;" >> %1.DbLib
+echo [OutputDatabaseLinkFile] > %file%.DbLib
+echo Version=1.1 >> %file%.DbLib
+echo [DatabaseLinks] >> %file%.DbLib
+for /f %%f in ("%file%.db") do (
+	echo ConnectionString=Provider=MSDASQL.1;Persist Security Info=False;Extended Properties="Driver={SQLite3 ODBC Driver};Database=%%~ff;StepAPI=0;SyncPragma=;NoTXN=0;Timeout=;ShortNames=0;LongNames=0;NoCreat=0;NoWCHAR=0;FKSupport=0;JournalMode=;OEMCP=0;LoadExt=;BigInt=0;JDConv=0;" >> %file%.DbLib
 )	
-echo AddMode=3 >> %1.DbLib
-echo RemoveMode=1 >> %1.DbLib
-echo UpdateMode=2 >> %1.DbLib
-echo ViewMode=0 >> %1.DbLib
-echo LeftQuote=[ >> %1.DbLib
-echo RightQuote=] >> %1.DbLib
-echo QuoteTableNames=1 >> %1.DbLib
-echo UseTableSchemaName=0 >> %1.DbLib
-echo DefaultColumnType=VARCHAR(255) >> %1.DbLib
-echo LibraryDatabaseType= >> %1.DbLib
-echo LibraryDatabasePath= >> %1.DbLib
-echo DatabasePathRelative=0 >> %1.DbLib
-echo TopPanelCollapsed=0 >> %1.DbLib
+echo AddMode=3 >> %file%.DbLib
+echo RemoveMode=1 >> %file%.DbLib
+echo UpdateMode=2 >> %file%.DbLib
+echo ViewMode=0 >> %file%.DbLib
+echo LeftQuote=[ >> %file%.DbLib
+echo RightQuote=] >> %file%.DbLib
+echo QuoteTableNames=1 >> %file%.DbLib
+echo UseTableSchemaName=0 >> %file%.DbLib
+echo DefaultColumnType=VARCHAR(255) >> %file%.DbLib
+echo LibraryDatabaseType= >> %file%.DbLib
+echo LibraryDatabasePath= >> %file%.DbLib
+echo DatabasePathRelative=0 >> %file%.DbLib
+echo TopPanelCollapsed=0 >> %file%.DbLib
 set pp=%~dp0
 set pp=%pp:~0,-1%
-echo LibrarySearchPath=%pp% >> %1.DbLib
-echo OrcadMultiValueDelimiter=, >> %1.DbLib
-echo SearchSubDirectories=1 >> %1.DbLib
-echo SchemaName=ALTIUM >> %1.DbLib
+echo LibrarySearchPath=%pp% >> %file%.DbLib
+echo OrcadMultiValueDelimiter=, >> %file%.DbLib
+echo SearchSubDirectories=1 >> %file%.DbLib
+echo SchemaName=ALTIUM >> %file%.DbLib
 
 set n=1
 
-for /f "usebackq" %%i in (`%SQLITE3_PATH%\sqlite3.exe %1.db "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%%';"`) do (
+for /f "usebackq" %%i in (`%SQLITE3_PATH%\sqlite3.exe %file%.db "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%%';"`) do (
 	echo import %%i
-	%SQLITE3_PATH%\sqlite3.exe -csv %1.db ".import csv\\%%i.csv %%i"
+	%SQLITE3_PATH%\sqlite3.exe -csv %file%.db ".import csv\\%%i.csv %%i"
 
-	echo [Table!n!] >> %1.DbLib
-	echo SchemaName= >> %1.DbLib
-	echo TableName=%%i >> %1.DbLib
-	echo Enabled=True >> %1.DbLib
-	echo UserWhere=0 >> %1.DbLib
-	echo UserWhereText= >> %1.DbLib
+	echo [Table!n!] >> %file%.DbLib
+	echo SchemaName= >> %file%.DbLib
+	echo TableName=%%i >> %file%.DbLib
+	echo Enabled=True >> %file%.DbLib
+	echo UserWhere=0 >> %file%.DbLib
+	echo UserWhereText= >> %file%.DbLib
 	
 	set /a n+=1
 )
 
 set m=1
 
-for /f "usebackq" %%i in (`%SQLITE3_PATH%\sqlite3.exe %1.db "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%%';"`) do (
+for /f "usebackq" %%i in (`%SQLITE3_PATH%\sqlite3.exe %file%.db "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%%';"`) do (
 	echo create FieldMaps for %%i
 
-	for /f "usebackq" %%t in ( `c:\sqlite\sqlite3.exe %1.db "pragma table_info(%%i);" ^| findstr /C:"Part Name"` ) do (
+	for /f "usebackq" %%t in ( `c:\sqlite\sqlite3.exe %file%.db "pragma table_info(%%i);" ^| findstr /C:"Part Name"` ) do (
 		if not %%t=="" (
-			echo [FieldMap!m!] >> %1.DbLib
-			echo Options=FieldName=%%i.Part Name^|TableNameOnly=%%i^|FieldNameOnly=Part Name^|FieldType=0^|ParameterName=Part Name^|VisibleOnAdd=False^|AddMode=0^|RemoveMode=0^|UpdateMode=0 >> %1.DbLib
+			echo [FieldMap!m!] >> %file%.DbLib
+			echo Options=FieldName=%%i.Part Name^|TableNameOnly=%%i^|FieldNameOnly=Part Name^|FieldType=0^|ParameterName=Part Name^|VisibleOnAdd=False^|AddMode=0^|RemoveMode=0^|UpdateMode=0 >> %file%.DbLib
 			set /a m+=1
 		)
 	)
 
-	for /f "usebackq" %%t in ( `c:\sqlite\sqlite3.exe %1.db "pragma table_info(%%i);" ^| findstr /C:"Description"` ) do (
+	for /f "usebackq" %%t in ( `c:\sqlite\sqlite3.exe %file%.db "pragma table_info(%%i);" ^| findstr /C:"Description"` ) do (
 		if not %%t=="" (
-			echo [FieldMap!m!] >> %1.DbLib
-			echo Options=FieldName=%%i.Description^|TableNameOnly=%%i^|FieldNameOnly=Description^|FieldType=1^|ParameterName=[Description]^|VisibleOnAdd=False^|AddMode=0^|RemoveMode=0^|UpdateMode=0 >> %1.DbLib
+			echo [FieldMap!m!] >> %file%.DbLib
+			echo Options=FieldName=%%i.Description^|TableNameOnly=%%i^|FieldNameOnly=Description^|FieldType=1^|ParameterName=[Description]^|VisibleOnAdd=False^|AddMode=0^|RemoveMode=0^|UpdateMode=0 >> %file%.DbLib
 			set /a m+=1
 		)
 	)
 
-	for /f "usebackq" %%t in ( `c:\sqlite\sqlite3.exe %1.db "pragma table_info(%%i);" ^| findstr /C:"Library Ref"` ) do (
+	for /f "usebackq" %%t in ( `c:\sqlite\sqlite3.exe %file%.db "pragma table_info(%%i);" ^| findstr /C:"Library Ref"` ) do (
 		if not %%t=="" (
-			echo [FieldMap!m!] >> %1.DbLib
-			echo Options=FieldName=%%i.Library Ref^|TableNameOnly=%%i^|FieldNameOnly=Library Ref^|FieldType=1^|ParameterName=[Library Ref]^|VisibleOnAdd=False^|AddMode=0^|RemoveMode=0^|UpdateMode=0 >> %1.DbLib
+			echo [FieldMap!m!] >> %file%.DbLib
+			echo Options=FieldName=%%i.Library Ref^|TableNameOnly=%%i^|FieldNameOnly=Library Ref^|FieldType=1^|ParameterName=[Library Ref]^|VisibleOnAdd=False^|AddMode=0^|RemoveMode=0^|UpdateMode=0 >> %file%.DbLib
 			set /a m+=1
 		)
 	)
 
-	for /f "usebackq" %%t in ( `c:\sqlite\sqlite3.exe %1.db "pragma table_info(%%i);" ^| findstr /C:"Footprint Ref"` ) do (
+	for /f "usebackq" %%t in ( `c:\sqlite\sqlite3.exe %file%.db "pragma table_info(%%i);" ^| findstr /C:"Footprint Ref"` ) do (
 		if not %%t=="" (
-			echo [FieldMap!m!] >> %1.DbLib
-			echo Options=FieldName=%%i.Footprint Ref^|TableNameOnly=%%i^|FieldNameOnly=Footprint Ref^|FieldType=1^|ParameterName=[Footprint Ref]^|VisibleOnAdd=False^|AddMode=0^|RemoveMode=0^|UpdateMode=0 >> %1.DbLib
+			echo [FieldMap!m!] >> %file%.DbLib
+			echo Options=FieldName=%%i.Footprint Ref^|TableNameOnly=%%i^|FieldNameOnly=Footprint Ref^|FieldType=1^|ParameterName=[Footprint Ref]^|VisibleOnAdd=False^|AddMode=0^|RemoveMode=0^|UpdateMode=0 >> %file%.DbLib
 			set /a m+=1
 		)
 	)
 
-	for /f "usebackq" %%t in ( `c:\sqlite\sqlite3.exe %1.db "pragma table_info(%%i);" ^| findstr /C:"PCB3D Ref"` ) do (
+	for /f "usebackq" %%t in ( `c:\sqlite\sqlite3.exe %file%.db "pragma table_info(%%i);" ^| findstr /C:"PCB3D Ref"` ) do (
 		if not %%t=="" (
-			echo [FieldMap!m!] >> %1.DbLib
-			echo Options=FieldName=%%i.PCB3D Ref^|TableNameOnly=%%i^|FieldNameOnly=PCB3D Ref^|FieldType=1^|ParameterName=[PCB3D Ref]^|VisibleOnAdd=False^|AddMode=0^|RemoveMode=0^|UpdateMode=0 >> %1.DbLib
+			echo [FieldMap!m!] >> %file%.DbLib
+			echo Options=FieldName=%%i.PCB3D Ref^|TableNameOnly=%%i^|FieldNameOnly=PCB3D Ref^|FieldType=1^|ParameterName=[PCB3D Ref]^|VisibleOnAdd=False^|AddMode=0^|RemoveMode=0^|UpdateMode=0 >> %file%.DbLib
 			set /a m+=1
 		)
 	)
