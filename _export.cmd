e8dc9f9182c8324ccefa58990c0dc0c5757faf9a
s33274 <a.vasiliev@kmz.su>
2021-09-14 16:35:27 +0300

    Исправлена ошибка с переменной SQLITE3_PATH

--- _export.cmd
+++ _export.cmd
@@ -1,14 +1,15 @@
 @echo off
-rem set SQLITE3_PATH=C:\sqlite
+
 set file=gorizont
+set PATH=%SQLITE3_PATH%;%PATH%
 
 if not exist csv mkdir csv
 
 if exist csv\query.sql del /q csv\query.sql
 
-for /f "usebackq" %%i in (`%SQLITE3_PATH%\sqlite3.exe %file%.db "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%%';"`) do (
+for /f "usebackq" %%i in (`sqlite3.exe %file%.db "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%%';"`) do (
 	echo export %%i
 	echo DROP TABLE IF EXISTS %%i; >> csv\query.sql
-	%SQLITE3_PATH%\sqlite3.exe %file%.db ".schema %%i" >> csv\query.sql
-	%SQLITE3_PATH%\sqlite3.exe -noheader -csv %file%.db "select * from %%i;" > csv\%%i.csv
+	sqlite3.exe %file%.db ".schema %%i" >> csv\query.sql
+	sqlite3.exe -noheader -csv %file%.db "select * from %%i;" > csv\%%i.csv
 )
